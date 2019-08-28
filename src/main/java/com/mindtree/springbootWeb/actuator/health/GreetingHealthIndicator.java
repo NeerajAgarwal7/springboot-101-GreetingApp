4b8d07305e019127717c8cf9aab735ad13dd24a2
NeerajAgarwal7 <neeraj.agarwal2@mindtree.com>
2019-08-28 11:57:15 +0530

    customized health and info actuators

---
+++ src/main/java/com/mindtree/springbootWeb/actuator/health/GreetingHealthIndicator.java
@@ -0,0 +1,31 @@
+package com.mindtree.springbootWeb.actuator.health;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+import com.mindtree.springbootWeb.model.Greeting;
+import com.mindtree.springbootWeb.service.GreetingService;
+
+@Component
+public class GreetingHealthIndicator implements HealthIndicator {
+
+	@Autowired
+	private GreetingService greetingService;
+	
+	@Override
+	public Health health() {
+
+		Collection<Greeting> greetings = greetingService.findAll();
+		
+		if(greetings == null || greetings.size() == 0) {
+			return Health.down().withDetail("count", 0).build();
+		}
+		
+		return Health.up().withDetail("count", greetings.size()).build();
+	}
+
+}
